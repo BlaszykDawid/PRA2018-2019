d33bc87a99d85144d183dcb190c679dc86d85432
DawidB <dawbla@st.amu.edu.pl>
2018-10-12 14:58:09 +0200

    Zajecia 2 chkpt 1

--- PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,6 +1,32 @@
 package second.shortcuts;
 
+import java.util.List;
+
 public class ClassThatHaveItAll implements InterfaceOne {
 
-    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class); String name; Integer number; public void printMe(String info) { logger.info(info); } public void usageOfPrint() { printMe("Hi"); }
+    List<Long> list;
+
+    public void setList(List<Long> list) {
+        this.list = list;
+    }
+
+    public List<Long> getList() {
+        return list;
+    }
+
+    public ClassThatHaveItAll(List<Long> list) {
+        this.list = list;
+    }
+
+    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class);
+    String name;
+    Integer number;
+
+    public void printMe(String info) {
+        logger.info(info);
+    }
+
+    public void usageOfPrint() {
+        printMe("Hi");
+    }
 }
