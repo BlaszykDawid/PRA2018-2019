dfea44df39ce51f49d0f5c564d85608fbce7e595
DawidB <dawbla@st.amu.edu.pl>
2018-10-12 14:47:50 +0200

    Zajecia 1

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,13 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    final static Logger a = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        a.info("message");
     }
 }
